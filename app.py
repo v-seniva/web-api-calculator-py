2e541d002934f50e13a2d79ad281ad5d87d9554c
v-seniva <violeta.seniva@gmail.com>
2022-03-01 20:51:36 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from importlib.resources import open_text
+from wsgiref import headers
 from click import open_file
 from flask import Flask
 from flask import request
@@ -7,33 +8,73 @@ from functions import is_number
 
 app = Flask(__name__)
 
-# import the math module 
-# import math 
+# import the math module
+# import math
+
 
 @app.route("/", methods=['GET'])
 def root():
     print('WEB-API-CALCULTOR-PY')
-    
+
     # Open a file: file
-    file = open('README.md',mode='r')
- 
+    file = open('README.md', mode='r')
+
     # read all lines at once
     all_of_it = file.read()
- 
+
     # close the file
     file.close()
     print(all_of_it.splitlines())
     return '<br>'.join(all_of_it.splitlines())
 
-   
-
 
 # curl -d '{"input1": 1, "input2": 2, "operator": "/"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/calculator
-@app.route("/calculator", methods=['POST'])
+@app.route("/calculator", methods=['GET', 'POST'])
 def calculator():
-    #curl -d  -X  http://127.0.0.1:5000/calculator
-    #if request.headers['Content-Type'] != 'application/json; charset=UTF-8':
-    #     return 'Bad request. JSON required. Error 1\n'
+    # 1. https://stackoverflow.com/questions/29386995/how-to-get-http-headers-in-flask
+    if 'Content-Type' not in request.headers:
+       return '`Content-Type` is required. Error: 1'
+    
+    # 2. https://stackoverflow.com/questions/23714383/what-are-all-the-possible-values-for-http-content-type-header
+    # 2. Encoding
+    # if request.headers['Content-Type'] != 'application/json' or request.headers['Content-Type'] != 'application/json; charset=utf-8':
+    #     print(request.headers['Content-Type'])
+    #     return '`Content-Type` must be `application/json`. Error: 2'
+    
+    if request.headers['Content-Type'] not in ['application/json', 'application/json; charset=utf-8']:
+        return '`Content-Type` must be `application/json`. Error: 2'                                
+    
+    """
+    3. Response:
+    https://developer.mozilla.org/ru/docs/Web/HTTP/Status
+    https://stackoverflow.com/questions/45412228/sending-json-and-status-code-with-a-flask-response
+    """
+    
+    json = request.get_json()
+    if not json:
+        # 3.1 Simple
+        dic = {
+            "Example below": True,
+            "input1": "1000",
+            "input2": "2000",
+            "operator": "*"
+        }
+        return dic,415
+        # print(dic['operator'] + dic['input1'] + dic['input2'])
+        # responseContent = JSON.dumps(dic)
+        
+        # # 3.2. Complicated
+        # responseContent = JSON.dumps({"input1": 1, "input2": 2, "operator": "/"})
+    
+        # response = app.response_class(
+        #     responseContent,
+        #     status=405,
+        #     mimetype='application/json'
+        # )
+        # return response
+        
+    # General logic
+    # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
     json = request.get_json()
     allowed_operators = ['+', '-', '*', '/']
     allowed_json_keys = ['input1', 'input2', 'operator']
@@ -43,7 +84,7 @@ def calculator():
     # curl -d '{"input1": 1, "input2": 1}' -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/calculator
     for key in allowed_json_keys:
         if key not in json:
-            return 'Bad request. Error: 2\n'
+            return 'Bad request. Error: 3\n'
     
     # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
     
@@ -57,7 +98,7 @@ def calculator():
         if isinstance(operand, int) or isinstance(operand, float):
             continue
         if not is_number(operand):
-            return 'Bad request. Error: 3\n'
+            return 'Bad request. Error: 4\n'
     
     # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
     
@@ -73,7 +114,7 @@ def calculator():
 
     # curl -d '{"input1": 1, "input2": 2, "operator": "sqrt"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/calculator
     if operator not in allowed_operators:
-        return 'Bad request. Error: 4\n'
+        return 'Bad request. Error: 5\n'
     
     # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
     
@@ -93,4 +134,4 @@ def calculator():
     elif operator == '/':
         return str(float(op1) / float(op2)) + '\n'  
     else:
-        return 'Bad request. Error: 5\n'
+        return 'Bad request. Error: 6\n'
